628fadb58ef1a05c2329ae47cff2a2aa89020ed6
SzymonWelter <szymon.welter98@gmail.com>
2019-02-10 23:01:27 +0100

    prototype of intro page was created

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {Navbar, Button, Container} from "react-bootstrap";
+import {Navbar, Button, Container, Row, Col} from "react-bootstrap";
 
 export const MainNav = (props) => {
     return (
-        <Container fluid style={{ paddingLeft: 0, paddingRight: 0}}>
-            <Navbar bg="dark" variant="dark" >
+        <Container fluid style={{paddingLeft: 0, paddingRight: 0, marginBottom: 100}}>
+            <Navbar bg="dark" variant="dark">
                 <Navbar.Collapse className="justify-content-center">
-                    <Navbar.Brand href="#home">LOGO</Navbar.Brand>
+                    <Navbar.Brand href="/">LOGO</Navbar.Brand>
                 </Navbar.Collapse>
                 <Navbar.Collapse className="justify-content-end">
                     <Button variant="outline-info">Log In</Button>
                 </Navbar.Collapse>
             </Navbar>
-
         </Container>
     );
 };
