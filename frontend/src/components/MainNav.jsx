bdaa0120ca84205e24152c0caeca14804482458c
SzymonWelter <szymon.welter98@gmail.com>
2019-02-12 21:38:00 +0100

    Logo centered

--- frontend/src/components/MainNav.jsx
+++ frontend/src/components/MainNav.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import {Navbar, Button, Container, Row, Col} from "react-bootstrap";
+import {Navbar, Nav} from "react-bootstrap";
+
 
 export const MainNav = (props) => {
     return (
-        <Container fluid style={{paddingLeft: 0, paddingRight: 0, marginBottom: 50}}>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Collapse className="justify-content-center">
-                    <Navbar.Brand href="/">LOGO</Navbar.Brand>
-                </Navbar.Collapse>
-                <Navbar.Collapse className="justify-content-end">
-                    <Button variant="outline-info">Log In</Button>
-                </Navbar.Collapse>
-            </Navbar>
-        </Container>
+        <Navbar bg="dark" variant="dark" expand={"lg"} style={{position: "relative", marginBottom: 50}}>
+            <Navbar.Brand style={{position: "absolute", left: "50%",transform: "translateX(-50%)" }} href="/">
+            <img
+                className="d-block w-100"
+                src="holder.js/200x40?text=LOGO&bg=F1A355"
+                alt="LOGO"
+            />
+            </Navbar.Brand>
+            <Nav/>
+            <Navbar.Toggle aria-controls="basic-navbar-nav justify-content-end"/>
+            <Navbar.Collapse className="justify-content-end" id={"basic-navbar-nav"}>
+                <Nav className="mr-auto">
+                </Nav>
+                /*TODO responsive dropdown login form*/
+
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
